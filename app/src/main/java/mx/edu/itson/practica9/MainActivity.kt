ac0a55709a9e017ec495a615bc3c4584dc1b5f80
CharlyOwo81 <gamaliel.armenta245254@potros.itson.edu.mx>
2025-03-18 18:55:27 -0700

    Primer Commit

---
+++ app/src/main/java/mx/edu/itson/practica9/MainActivity.kt
@@ -0,0 +1,19 @@
+package mx.edu.itson.practica9
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        var btnSave: Button =findViewById(R.id.save_button) as Button
+
+    }
+}
